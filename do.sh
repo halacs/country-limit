7f84861743b7ebf9d82b44656e5b255adff5ec80
Nyíri Gábor <halacs@users.noreply.github.com>
2024-05-16 21:56:26 +0200

    bugfix

--- do.sh
+++ do.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# vim: ts=3
 
 COUNTRIES=('hu' 'de')	# only these countries can connect to below ports
 PORTS=('993' '21115' '21116' '21117')	# IMAP, RustDesk
-IFACE="enp0s3"	# internet facing network interface
+IFACE="ens3"	# internet facing network interface
 
 BASE_DIR="$(dirname $0)"
 COUNTRIES_DIR="${BASE_DIR}/countries"
@@ -22,25 +23,30 @@ function downloadCountries() {
 }
 
 function updatesCountryList() {
+	v4listAllowed="countries_allowed_v4"
+	v6listAllowed="countries_allowed_v6"
+
+	echo Creating ipsets
+	ipset -exist create "${v4listAllowed}" hash:net family inet
+	ipset -exist create "${v6listAllowed}" hash:net family inet6
+
+	echo Flushing ipsets
+	ipset flush ${v4listAllowed}
+	ipset flush ${v6listAllowed}
+
 	for country in "${COUNTRIES[@]}"; do
 		echo Country: ${country}
 
 		#v4list="countries_${country}_v4"
-		v4listAllowed="countries_allowed_v4"
 		#v6list="countries_${country}_v6"
-		v6listAllowed="countries_allowed_v6"
 
-		echo Creating ipsets
+		#echo Creating ipsets
 		#ipset -exist create "${v4list}" hash:net family inet
-		ipset -exist create "${v4listAllowed}" hash:net family inet
 		#ipset -exist create "${v6list}" hash:net family inet6
-		ipset -exist create "${v6listAllowed}" hash:net family inet6
 
-		echo Flushing ipsets
+		#echo Flushing ipsets
 		#ipset flush ${v4list}
-		ipset flush ${v4listAllowed}
 		#ipset flush ${v6list}
-		ipset flush ${v6listAllowed}
 
 		echo Adding IPv4 addresses
 		for ip in $(cat ${COUNTRIES_DIR}/${country}.ipv4);  do
@@ -54,6 +60,11 @@ function updatesCountryList() {
         ipset add "${v6listAllowed}" "${ip}"
    	done
 	done
+
+	if [ "${1}" == "update" ]; then
+		echo 'Saving ipsets into ipset-persistent (/etc/iptables/ipsets)'
+		ipset save > /etc/iptables/ipsets
+	fi
 }
 
 function initIpTables() {
@@ -76,17 +87,17 @@ function initIpTables() {
 
 		# IPv4 - tcp
 		iptables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j DROP -m comment --comment "Block unknown country (v4)"
-		iptables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j LOG -m comment --comment "Block unknown country (v4)" --log-prefix "Block unknown country "
+		iptables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j LOG -m comment --comment "Block unknown country (v4)" --log-prefix "Block unknown country"
 		# IPv4 - udp
 		iptables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j DROP -m comment --comment "Block unknown country (v4)"
-		iptables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j LOG -m comment --comment "Block unknown country (v4)" --log-prefix "Block unknown country "
+		iptables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v4listAllowed}" src -j LOG -m comment --comment "Block unknown country (v4)" --log-prefix "Block unknown country"
 	
 		# IPv6 - tcp
 		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j DROP -m comment --comment "Block unknown country (v6)"
-		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j LOG -m comment --comment "Block unknown country (v6)" --log-prefix "Block unknown country "
+		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p tcp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j LOG -m comment --comment "Block unknown country (v6)" --log-prefix "Block unknown country"
 		# IPv6 - udp
 		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j DROP -m comment --comment "Block unknown country (v6)"
-		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j LOG -m comment --comment "Block unknown country (v6)" --log-prefix "Block unknown country "
+		ip6tables -I ${CHAIN_NAME} -i ${IFACE} -p udp --dport ${port} -m set ! --match-set "${v6listAllowed}" src -j LOG -m comment --comment "Block unknown country (v6)" --log-prefix "Block unknown country"
 	done
 }
 
